3860861fb95c2b008028d7a44b3c0f1bdc0ec0bb
Philip Skaria <philipskaria04@gmail.com>
2025-07-22 21:47:06 +0530

    debugging

--- src/background/index.ts
+++
@@ -1,6 +0,0 @@
-// src/background/index.ts
-console.log('LeetSync: Background script loaded');
-
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('LeetSync: Extension installed');
-});
